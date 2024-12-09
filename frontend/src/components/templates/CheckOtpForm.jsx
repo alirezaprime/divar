1a2af30d23345b555d1fb2d640ee4c913eb5016c
alireza tabatabaeei poor <tabatabaeei.alireza@gmail.com>
2024-12-09 11:08:32 +0330

    redirect user after login

--- frontend/src/components/templates/CheckOtpForm.jsx
+++ frontend/src/components/templates/CheckOtpForm.jsx
@@ -1,7 +1,14 @@
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+
 import { checkOtp } from "services/auth";
-import { setCookie } from "src/utils/cookie";
+import { getProfile } from "services/user";
+import { setCookie } from "utils/cookie";
 
 function CheckOtpForm({ mobile, code, setCode, setStep }) {
+  const { refetch } = useQuery(["profile"], getProfile);
+  const navigate = useNavigate();
+
   const submitHandler = async (event) => {
     event.preventDefault();
     // console.log({ code, mobile });
@@ -10,6 +17,8 @@ function CheckOtpForm({ mobile, code, setCode, setStep }) {
     const { response, error } = await checkOtp(code, mobile);
     if (response) {
       setCookie(response.data);
+      navigate("/");
+      refetch();
     }
     if (error) toast("مشکلی وجود دارد ");
   };
--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -1,4 +1,8 @@
 import axios from "axios";
+// import { request } from "axios";
+import { getNewTokens } from "services/token";
+import { setCookie } from "src/utils/cookie";
+import { getCookie } from "utils/cookie";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
@@ -6,4 +10,37 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  (request) => {
+    const accessToken = getCookie("accessToken");
+    if (accessToken) {
+      request.headers["Authorization"] = `bearer ${accessToken}`;
+    }
+    return request;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    // console.log(originalRequest);
+    if (error.response.status === 401 && !request._retry) {
+      originalRequest._retry = true;
+
+      const res = await getNewTokens();
+      if (!res?.response) return;
+      setCookie(res.response.data);
+      console.log(res);
+
+      return api(originalRequest);
+    }
+  }
+);
 export default api;
